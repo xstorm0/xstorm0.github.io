88c020baff2829eb2a88a6207f8f3ef636357793
NioBima <ccss.cs363@gmail.com>
2025-11-26 19:20:25 +0700

    1

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ var start_x='xxxx';
 // The HEN payload will check this value so it knows whether or not to remove boot_plugins.txt
 var hen_installer_bytes=0x48454E00;
 
-var usb_sp='/dev_usb000/' + fwVersion + '/PS3HEN.p3t';
+// Multi USB support
+var usb_sp0 = '/dev_usb000/' + fwVersion + '/PS3HEN.p3t';
+var usb_sp1 = '/dev_usb001/' + fwVersion + '/PS3HEN.p3t';
+
+// Gabungkan kedua path
+var usb_sp = usb_sp0 + usb_sp1;
 var mount_path='/dev_blind';
 var flash_partition='xxxxCELL_FS_IOS:BUILTIN_FLSH1';
 var filesystem='CELL_FS_FAT';
